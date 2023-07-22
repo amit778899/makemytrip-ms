bc33d64d2a332f7c08b817d076f161a1a6776dba
Amit Deshpande <amit.deshpande@sungardas.com>
2023-07-22 16:44:03 +0530

    added bus module

---
+++ src/main/java/com/example/makemytripms/bus.java
@@ -0,0 +1,10 @@
+package com.example.makemytripms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class bus {
+    @GetMapping("/bus")
+    public String getData() {return  "Please book bus from 200 to 500" ; }
+}
