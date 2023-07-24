5d7fb99f0c7450ff1e98483ea62fd025207c18a6
Amit Deshpande <amit.deshpande@sungardas.com>
2023-07-24 22:33:22 +0530

    adding holidays module

---
+++ src/main/java/com/example/makemytripms/holidays.java
@@ -0,0 +1,10 @@
+package com.example.makemytripms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class holidays {
+    @GetMapping("/holidays")
+    public String getData() {return  "Please book holidays package" ; }
+}
