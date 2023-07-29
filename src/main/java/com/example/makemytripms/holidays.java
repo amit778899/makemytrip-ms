6294ca832e18c446f5845fbd8400f5910e3d9cba
amit778899 <139779369+amit778899@users.noreply.github.com>
2023-07-29 15:40:39 +0530

    Update holidays.java

--- src/main/java/com/example/makemytripms/holidays.java
+++ src/main/java/com/example/makemytripms/holidays.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class holidays {
     @GetMapping("/holidays")
-    public String getData() {return  "Please book holidays package" ; }
+    public String getData() {return  "Please book holidays package A B C D" ; }
 }
