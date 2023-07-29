4132ec85a2220ae47f9ede05700a9f1a844e34ab
Amit Deshpande <amit.deshpande@sungardas.com>
2023-07-29 15:50:11 +0530

    modified holiday module

--- src/main/java/com/example/makemytripms/holidays.java
+++ src/main/java/com/example/makemytripms/holidays.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class holidays {
     @GetMapping("/holidays")
-<<<<<<< HEAD
     public String getData() {return  "Please book holidays package A B C D" ; }
-=======
-    public String getData() {return  "Please book holidays package A B C D" ; }
->>>>>>> 6294ca832e18c446f5845fbd8400f5910e3d9cba
+
 }
