530199895412fdcb79d96931d0c9835d026c6bb6
Amit Deshpande <amit.deshpande@sungardas.com>
2024-03-05 21:57:35 +0530

    change in flight MS

--- src/main/java/com/example/makemytripms/flight.java
+++ src/main/java/com/example/makemytripms/flight.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class flight {
     @GetMapping("/flight")
-    public String getData() {return  "Please book flight from delhi to Pune" ; }
+    public String getData() {return  "Please book flight from Delhi to Pune" ; }
 }
